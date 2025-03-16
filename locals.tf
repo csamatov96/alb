e00f7e6635fc4706e6bd38785259b2574b6226e3
csamatov <50221223+csamatov96@users.noreply.github.com>
2025-03-15 23:05:19 -0500

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -98,72 +98,7 @@ locals {
     } : {}
   )
 
-  custom_listeners = {
-    for listener in var.custom_listeners : "${listener.protocol}-${listener.port}" => {
-      port                        = lookup(listener, "port", null)
-      priority = lookup(listener_rule, "priority", 1)
-      protocol                    = lookup(listener, "protocol", null)
-      certificate_arn             = lookup(listener, "certificate_arn", null)
-      ssl_policy                  = lookup(listener, "protocol", null) == "HTTPS" ? lookup(listener, "ssl_policy", "ELBSecurityPolicy-2016-08") : null
-      additional_certificate_arns = lookup(listener, "additional_certificate_arns", null)
 
-#      actions = {}
-
-      default_action = {
-        type = listener.default_action.type
-
-        configuration = {
-          target_group = listener.default_action.type == "forward" ? listener.default_action.target_group_key : null
-#          status_code  = listener.default_action.type == "redirect" ? listener.default_action.configuration.status_code : null
-#          host         = listener.default_action.type == "redirect" ? lookup(listener.default_action.configuration, "host", "#{host}") : null
-#          path         = listener.default_action.type == "redirect" ? lookup(listener.default_action.configuration, "path", "/#{path}") : null
-#          port         = listener.default_action.type == "redirect" ? lookup(listener.default_action.configuration, "port", "#{port}") : null
-#          protocol     = listener.default_action.type == "redirect" ? lookup(listener.default_action.configuration, "protocol", "#{protocol}") : null
-#          query        = listener.default_action.type == "redirect" ? lookup(listener.default_action.configuration, "query", "#{query}") : null
-
-        }
-      }
-
-      conditions = listener.conditions
-    }
-  }
-
-  # Get all of the additional certificates defined within the listeners and
-  # create a list with the information needed for the certificate resource.
-  additional_certificates = flatten([
-    for listener_name, listener_rule in local.custom_listeners : [
-      for cert_name, cert_arn in listener_rule.additional_certificate_arns : {
-        listener_name = listener_name
-        cert_name     = cert_name
-        cert_arn      = cert_arn
-      }
-    ] if contains(keys(listener_rule), "additional_certificate_arns") && listener_rule.additional_certificate_arns != null
-  ])
-
-  # Convert the list of additional certificates into a named map for the
-  # for_each statement to work with named instances.
-  additional_certificates_map = {
-    for values in local.additional_certificates : "${values.listener_name}_${values.cert_name}" => {
-      certificate_arn = values.cert_arn
-      listener_name   = values.listener_name
-    }
-  }
-
-  custom_listener_rules = {
-    for listener_name, listener_rule in local.custom_listeners : listener_name => {
-      priority = lookup(listener_rule, "priority", 1)
-
-      action = {
-        type = listener_rule.default_action.type
-
-        configuration = {
-          target_group = listener_rule.default_action.type == "forward" ? listener_rule.default_action.configuration.target_group : null
-        }
-      }
-
-      conditions = listener_rule.conditions
-    } if listener_rule.default_action != {}
-  }
 
   target_groups = length(var.target_groups) == 0 ? {
     default = {
